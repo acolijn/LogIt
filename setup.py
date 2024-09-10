b9d855a75e1611a19a83bfbb4c75336b9eb3896c
acolijn <auk.pieter.colijn@gmail.com>
2024-09-10 11:48:35 +0200

    docs

--- setup.py
+++ setup.py
@@ -7,32 +7,32 @@ setup(
     package_dir={'': 'app/routes'},  # This tells it that the packages are in src/
 )
 
-# if __name__ == "__main__":
-#     from app import create_app, mongo  # Move the imports here to avoid breaking setup
-#     from werkzeug.security import generate_password_hash
-#     import getpass
+if __name__ == "__main__":
+    from app import create_app, mongo  # Move the imports here to avoid breaking setup
+    from werkzeug.security import generate_password_hash
+    import getpass
     
-#     def create_admin():
-#         username = input("Enter admin username: ")
-#         password = getpass.getpass("Enter admin password: ")
+    def create_admin():
+        username = input("Enter admin username: ")
+        password = getpass.getpass("Enter admin password: ")
         
-#         hashed_password = generate_password_hash(password, method='scrypt')
-#         admin = {
-#             "username": username,
-#             "password": hashed_password,
-#             "is_admin": True
-#         }
+        hashed_password = generate_password_hash(password, method='scrypt')
+        admin = {
+            "username": username,
+            "password": hashed_password,
+            "is_admin": True
+        }
         
-#         # Insert the admin user into the 'users' collection
-#         result = mongo.db.users.insert_one(admin)
-#         if result:
-#             print(f"Admin user created with ID: {result.inserted_id}")
-#         else:
-#             print("Error creating admin user.")
+        # Insert the admin user into the 'users' collection
+        result = mongo.db.users.insert_one(admin)
+        if result:
+            print(f"Admin user created with ID: {result.inserted_id}")
+        else:
+            print("Error creating admin user.")
 
-#     # Initialize the Flask app
-#     app = create_app('config.Config')
+    # Initialize the Flask app
+    app = create_app('config.Config')
     
-#     # Use the app's context to ensure the MongoDB connection is active
-#     with app.app_context():
-#         create_admin()
+    # Use the app's context to ensure the MongoDB connection is active
+    with app.app_context():
+        create_admin()
